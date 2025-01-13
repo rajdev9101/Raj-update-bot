56cf2eba24db39e383757f89db452e8ec154321d
rajdev9101 <devraj93957@gmail.com>
2025-01-13 13:21:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -118,9 +118,9 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', False))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'thunderlinks.site')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'a1a367bab65fced5d41ed459a2142dbb0aab372d')
+VERIFY = bool(environ.get('VERIFY', True))
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'arolinks.com')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'dbf9516e171738e9f47008221e3fdce76ba5c6dd')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
