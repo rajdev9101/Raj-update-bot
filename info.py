079f439e9fa5fc9c18c95d0247de607a0ca004b1
rajdev9101 <devraj93957@gmail.com>
2025-06-29 23:17:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,4 +1,4 @@
-# Don't Remove Credit @VJ_Botz
+j# Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
@@ -88,7 +88,7 @@ OWNER_USERNAME = environ.get('OWNER_USERNAME', 'raj_dev_01') # owner username wi
 
 
 # Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
-CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
+CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone mode is true
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
